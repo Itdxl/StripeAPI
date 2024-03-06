330abf7f9a8af6a9051ced29461d8d95568bc471
Dmitry Shevchenko <logimor@yandex.com>
2024-03-06 14:06:40 +0300

    All Order Buy+list  + discounts started

--- stripe_project/stripe_app/urls.py
+++ stripe_project/stripe_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BuyItemView, ItemView, OrderView
+from .views import BuyItemView, ItemView, OrderView, CouponView, BuyOrderView
 
 from . import views
 # from rest_framework.routers import DefaultRouter
@@ -9,9 +9,14 @@ from . import views
 urlpatterns = [
     path('item/<int:pk>/', ItemView.as_view(), name='item-detail'),
     path('buy/<int:pk>/', BuyItemView.as_view(), name='buy-item'),
+    path('cart/', OrderView.as_view(), name='cart-list'),
+    path('add-to-cart/<int:item_id>/', OrderView.as_view(), name='add-to-cart'),
     path('success/', views.successful, name='success'),
     path('cancel/', views.canceled, name='cancel'),
-    path('add-to-cart/<int:item_id>/', OrderView.as_view(), name='add-to-cart'),
-    path('cart/', OrderView.as_view(), name='cart-list'),
+    # path('coupon/<int:pk>/', views.get_coupon, name ='get-coupon'),
+    # path('coupons/',  CouponView.as_view(), name='coupons'),
+
+    path('buy-cart/', BuyOrderView.as_view(), name='buy-item'),
+    path('buy-cart/<int:pk>/', BuyOrderView.as_view(), name='buy-all'),
 
 ]
--- stripe_project/stripe_app/views.py
+++ stripe_project/stripe_app/views.py
@@ -1,10 +1,9 @@
 import stripe 
 
-
-
+from django.db.models import Sum, F
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.conf import settings
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -15,81 +14,116 @@ from .serializers import OrderItemSerializer
 
 
 class OrderView(APIView):
-
     def get(self, request):
-        # Получаем или создаем заказ для текущего пользователя
         order, created = Order.objects.get_or_create()
-
-        # Получаем все элементы заказа для этого заказа
         order_items = OrderItem.objects.filter(order=order)
+        # берем queryset order_items
+        # делаем новое поле total_for_item и через anotate считаем общую цену
+        # anotate для каждого элемента(позиции) заказа
+        # aggregate считает общую сумму всех СУММ ПОЗИЦИЙ
+        # F используется для обращения к значениям полей
+        total = order_items.annotate(total_for_item=F('price') * F('quantity')) \
+                           .aggregate(total=Sum('total_for_item'))['total']
 
-        # Сериализуем элементы заказа
         serializer = OrderItemSerializer(order_items, many=True)
-
-        # Возвращаем ответ с данными о содержимом корзины
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        data = {
+            'order_items': serializer.data,
+            'total': total if total else 0
+        }
+        return Response(data, status=status.HTTP_200_OK)
     
 
     def post(self, request, item_id):
         item = get_object_or_404(Item, pk=item_id)
-        
-        # Проверяем, есть ли корзина у текущего пользователя или создаем новую
+        # Проверяем или создаем. (user=request.user), если делать пользователей
         order_instance, created = Order.objects.get_or_create()
         price = item.price
-
-        # Проверяем, есть ли уже такой товар в корзине, и увеличиваем его количество
         order_item_instance, created = OrderItem.objects.get_or_create(order=order_instance, item=item, price=price)
         if not created:
             order_item_instance.quantity += 1
             order_item_instance.save()
         
-        return Response({'message': 'Item added to Order successfully'})
+        return Response({'message': 'Added to order'})
     
 
 class ItemView(APIView):
-
     def get(self, request, pk):
         public_stripe_key = settings.PUBLIC_STRIPE_KEY
         item = get_object_or_404(Item, pk=pk)
         context = {
             'item': item,
-            # 'item_id': item.id,
             'public_stripe_key': public_stripe_key
         }
         return render(request, 'item_details.html', context)
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
+def create_stripe_session(amount):
+    try:
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                "price_data": {
+                    "currency": "eur",
+                    "product_data": {
+                        "name": "Purchase",  # Название товара или заказа
+                    },
+                    "unit_amount": int(amount * 100),  # Сумма в копейках
+                },
+                "quantity": 1,
+            }],
+            mode='payment',
+            success_url=settings.REDIRECT_DOMAIN + '/success',
+            cancel_url=settings.REDIRECT_DOMAIN + '/cancel',
+        )
+        return session.id
+    except Exception as e:
+        # Обработка ошибок создания сессии оплаты
+        return str(e)
 
-
+   
 class BuyItemView(APIView):
     def get(self, request, pk):
         item = get_object_or_404(Item, pk=pk)
-        # item_id = self.kwargs['pk']
-        # item = get_object_or_404(Item, pk=item_id)
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        
-        # Создание сессии оплаты Stripe
-        try:
-            session = stripe.checkout.Session.create(
-                payment_method_types=['card'],
-                line_items=[{
-                    "price_data": {
-                        "currency": "eur",
-                        "product_data": {
-                        "name": item.name,
-                    },
-                    "unit_amount": int(item.price* 100),
-                },
-                    "quantity": 1,
-                }],
-                mode='payment',
-                success_url = settings.REDIRECT_DOMAIN + '/success',
-			    cancel_url = settings.REDIRECT_DOMAIN + '/cancel',
-            )
+        session_id = create_stripe_session(item.price)
+        return Response({'session_id': session_id})
+    
+class BuyOrderView(APIView):
+     def get(self, request):
+        order, created = Order.objects.get_or_create()
+        order_items = OrderItem.objects.filter(order=order)
+        total = order_items.annotate(total_for_item=F('price') * F('quantity')) \
+                           .aggregate(total=Sum('total_for_item'))['total']
+        session_id = create_stripe_session(total)
+        return Response({'session_id': session_id})
+
+# class BuyItemView(APIView):
+#     def get(self, request, pk):
+#         item = get_object_or_404(Item, pk=pk)
+#         stripe.api_key = settings.STRIPE_SECRET_KEY
+#         # Создание сессии оплаты Stripe
+#         try:
+#             session = stripe.checkout.Session.create(
+#                 payment_method_types=['card'],
+#                 line_items=[{
+#                     "price_data": {
+#                         "currency": "eur",
+#                         "product_data": {
+#                         "name": item.name,
+#                     },
+#                     # Stripe не работает с десятичными числами
+#                     "unit_amount": int(item.price* 100),
+#                 },
+#                     "quantity": 1,
+#                 }],
+#                 mode='payment',
+#                 success_url = settings.REDIRECT_DOMAIN + '/success',
+# 			    cancel_url = settings.REDIRECT_DOMAIN + '/cancel',
+#             )
             
-            # Возвращаем идентификатор сессии оплаты
-            return Response({'session_id': session.id})
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+#             # Возвращаем идентификатор сессии оплаты
+#             return Response({'session_id': session.id})
+#         except Exception as e:
+#             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
 
 def successful(request):
@@ -98,3 +132,38 @@ def successful(request):
 
 def canceled(request):
 	return render(request, 'cancel.html')
+
+
+def get_coupon(self, pk):
+        item = get_object_or_404(Item, pk=pk)
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        try:
+            discount = item.discount.get(is_active=True)
+        except Exception:
+            return None
+
+        coupon = stripe.Coupon.create(
+            duration="once",
+            id=f"coupon-{item.id}",
+            percent_off=discount.amount,
+        )
+        if coupon:
+            return Response({'coupon': coupon.id})
+        else:
+            return Response({'error': 'Discount not found for item'}, status=status.HTTP_404_NOT_FOUND)
+
+            
+        
+class CouponView(APIView):
+    def get(self, request):
+        stripe.api_key = settings.STRIPE_SECRET_KEY        
+        coupons = stripe.Coupon.list()
+        coupon_ids = [[coupon.id,coupon.percent_off] for coupon in coupons.data]
+        return JsonResponse({'coupons': coupon_ids})
+
+    def delete(self, request):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        coupons = stripe.Coupon.list()
+        for coupon in coupons.data:
+            stripe.Coupon.delete(coupon.id)
+        return Response({'message': 'All coupons deleted successfully'})
